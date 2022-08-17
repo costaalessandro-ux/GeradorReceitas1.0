f282d2a4676878aac28d3b6a5fe9f137dbb2afd1
Alessandro <alessandrocosta103@gmail.com>
2022-08-17 12:14:31 -0300

    cria select de informações

---
+++ src/main/java/controller/selectInfos.java
@@ -0,0 +1,65 @@
+
+package controller;
+
+import dao.ReceitaDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Receita;
+
+public class selectInfos extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+       response.setContentType("text/html;charset=UTF-8");
+       
+         // listar infos
+   
+        ReceitaDao dao = new ReceitaDao();
+        ArrayList<Receita> list = dao.listar();
+        request.setAttribute("listarInfos", list);
+        RequestDispatcher rd = request.getRequestDispatcher("pages/selectInfos.jsp");
+        rd.forward(request, response);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(selectInfos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(selectInfos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(selectInfos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(selectInfos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
